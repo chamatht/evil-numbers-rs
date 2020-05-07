705e2fc071a177770bab67562aeddbc1eb681ff3
chamatht <chamatht@gmail.com>
2020-05-07 17:29:07 +0200

    add start stop values

--- src/libpr.rs
+++ src/libpr.rs
@@ -21,12 +21,19 @@ extern "C" {
     fn primesieve_next_prime_wa(it: *mut primesieve_iterator) -> u64;
     fn primesieve_init(it: *mut primesieve_iterator);
     fn primesieve_free_iterator(it: *mut primesieve_iterator);
+    fn primesieve_skipto(it: *mut primesieve_iterator, start: u64, stop_hint: u64);
+    fn primesieve_get_max_stop() -> u64;
 }
 
 impl primesieve_iterator {
-    pub fn new() -> primesieve_iterator {
+    pub fn new(start: u64) -> primesieve_iterator {
+        Self::new_with_stop(start, unsafe{primesieve_get_max_stop()})
+    }
+
+    pub fn new_with_stop(start: u64, stop_hint: u64) -> primesieve_iterator {
         let mut itr: primesieve_iterator = unsafe { mem::zeroed() };
         unsafe { primesieve_init(&mut itr) };
+        unsafe { primesieve_skipto(&mut itr, start, stop_hint) };
 
         itr
     }
@@ -38,7 +45,7 @@ impl Iterator for primesieve_iterator {
     fn next(&mut self) -> Option<Self::Item> {
         let p: u64 = unsafe { primesieve_next_prime_wa(self) };
         
-        if p == u64::MAX {
+        if p >= self.stop_hint {
             None
         } else {
             Some(p)
